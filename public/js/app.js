593be82a4a06790792f67a3de061d0bb086a7994
mohammad samara <mohammad.ab.samara@gmail.com>
2020-08-16 10:34:59 +0300

    upgrade search matches

--- public/js/app.js
+++ public/js/app.js
@@ -63,3 +63,23 @@ function openMatches(evt) {
   // evt.currentTarget.backgroundColor = 'red';
 }
 
+// search matches tabs
+document.getElementsByClassName('searchForm')[0].style.display = 'flex'; //displays the first leage matches section
+function openMatches(evt) {
+  var i, x, tablinks;
+
+  var selectedClassName = evt.currentTarget.classList[1];
+  var selectedId = evt.currentTarget.id;
+
+  x = document.getElementsByClassName("searchForm");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("single-search-name");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].style.backgroundColor = 'black';
+  }
+  document.getElementsByClassName(`${selectedClassName}`)[1].style.display = "flex";
+  document.getElementById(`${selectedId}`).style.backgroundColor = 'red';
+  // evt.currentTarget.backgroundColor = 'red';
+}
